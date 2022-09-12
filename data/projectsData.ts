71c859995b0199b574c1c153ebc3eb4851ecd9c4
hoony <hoonyland.newsletter@gmail.com>
2022-09-12 14:27:11 +0900

    update projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,9 +1,21 @@
 const projectsData = [
   {
     title: 'ogk',
-    description: `cli tool for open.go.kr`,
+    description: 'CLI for open.go.kr',
     imgSrc: '/static/images/projects/ogk.png',
     href: 'https://github.com/opengirok/ogk',
+  },
+  {
+    title: 'Tango Pentägono',
+    description: 'tango music concert @ftto',
+    imgSrc: '/static/images/projects/tango-pentagono.png',
+    href: 'https://tango-pentagono.ftto.kr',
+  },
+  {
+    title: 'Chanson Pentagon',
+    description: 'chanson music concert @ftto',
+    imgSrc: '/static/images/projects/chanson-pentagon.png',
+    href: 'https://chanson-pentagon.ftto.kr',
   },
 ]
 
